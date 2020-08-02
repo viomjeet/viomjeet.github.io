f3d5e8fc13d392b2756ea9496625426240f5171e
vikas kumar <viomjeet@users.noreply.github.com>
2020-08-02 15:20:25 +0530

    Update react_babel.js

--- js/react_babel.js
+++ js/react_babel.js
@@ -18,27 +18,100 @@ class Content extends React.Component {
         super();
         this.state = {
             pagetitle: "VIKAS KUMAR",
-            pagebody: 'Front End Developer',
-            social: []
-        }
-        fetch('./js/social.json').then(response => response.json())
-            .then(data => {
-                this.setState({ social: data.map(items => items) });
-            })
-            .catch(error => console.log(error));
+            pagebody: 'Freelancer, Front End Developer, front-end ui developer',
+            social: [],
+            projects: [
+                { "title": "xtbox", "datarel": ["noopener,xtbox"], "datatarget": "_blank", "url": "https://viomjeet.net/xtbox", "skills": ["React", "HTML5", "SASS"] },
+                { "title": "xtp", "datarel": ["noopener,xtp"], "datatarget": "_blank", "url": "https://viomjeet.net/xtp/", "skills": ["React", "HTML5", "SASS"] },
+                { "title": "jstst", "datarel": ["noopener,jstst"], "datatarget": "_blank", "url": "https://viomjeet.net/jstst", "skills": ["jQuery", "HTML5", "CSS"] },
+                { "title": "breakingbad", "datarel": ["noopener,breakingbad"], "datatarget": "_blank", "url": "https://viomjeet.net/breakingbad", "skills": ["React", "React-Redux", "HTML5", "SASS"] },
+                { "title": "redux_1", "datarel": ["noopener,redux_1"], "datatarget": "_blank", "url": "https://viomjeet.net/redux_1", "skills": ["React", "React-Redux", "HTML5", "Bootstrap"] },
+                { "title": "redux_2", "datarel": ["noopener,redux_2"], "datatarget": "_blank", "url": "https://viomjeet.net/redux_2", "skills": ["React", "React-Redux", "HTML5", "Bootstrap"] },
+                { "title": "ecart", "datarel": ["noopener,ecart"], "datatarget": "_blank", "url": "https://viomjeet.net/ecart", "skills": ["React", "HTML5", "CSS"] },
+                { "title": "ReactCRUDAPP", "datarel": ["noopener,ReactCRUDAPP"], "url": "https://viomjeet.net/ReactCRUDAPP", "skills": ["React", "HTML5", "CSS", "Bootstrap"] }
+            ],
+            skills: [
+                { "title": "React.js", "img": "react.png", "progress": "50", "bg": "bg-success" },
+                { "title": "React-Redux", "img": "redux.png", "progress": "30", "bg": "bg-warning" },
+                { "title": "javascript", "img": "js.png", "progress": "70", "bg": "bg-danger" },
+                { "title": "jquery.js", "img": "jquery.png", "progress": "80", "bg": "bg-info" },
+                { "title": "HTML5", "img": "html.png", "progress": "80", "bg": "bg-secondary" },
+                { "title": "CSS", "img": "css2.png", "progress": "80", "bg": "bg-primary" },
+                { "title": "SCSS", "img": "sass.png", "progress": "60", "bg": "bg-danger" },
+                { "title": "Bootstrap", "img": "bs.png", "progress": "60", "bg": "bg-warning" },
+                { "title": "JSON", "img": "json.png", "progress": "70", "bg": "bg-info" },
+                { "title": "API integration", "img": "api.png", "progress": "70", "bg": "bg-secondary" }
 
+            ],
+        }
+        const fetchApi = async () => {
+            const fetchData = await fetch('./js/social.json').then(response => response.json()).catch(error => { console.log(error) })
+            this.setState({ social: fetchData })
+        }
+        fetchApi();
     }
     render() {
         const { social } = this.state;
+        const { projects } = this.state;
+        const { skills } = this.state;
         return (
-            <div className="center_content">
-                <h1 className="page_title">VIKAS KUMAR</h1>
-                <p className="page_body">Front End Developer</p>
-                <ul className="social_icons">
-                    {social.map((items, index) => {
-                        return (<li key={index}><a target={items.datatarget} href={items.datahref} rel={items.datarel[0], items.datarel[1]} title={items.title}><i className={items.icon[0] + " " + items.icon[1]}></i></a></li>)
-                    })}
-                </ul>
+            <div className="container">
+                <div className="center_content">
+                    <h1 className="page_title">{this.state.pagetitle}</h1>
+                    <p className="page_body">{this.state.pagebody}</p>
+                    <div className="boxPanel">
+                        <h4 className="text-center">Latest Projectes</h4>
+                        <ul className="projects">
+                            {projects.map((items, index) => {
+                                return (<li key={index}>
+                                    <a target={items.datatarget} href={items.url} rel={items.datarel[0], items.datarel[1]}>
+                                        <div className="projectName"><strong>{items.title}</strong></div>
+                                        <ul>
+                                            {items.skills.map(skills => (
+                                                <li key={skills}>{skills}</li>
+                                            ))}
+                                        </ul>
+                                    </a>
+                                </li>)
+                            })}
+                        </ul>
+                    </div>
+
+                    <div className="boxPanel">
+                        <h4 className="text-center">Skills</h4>
+                        <ul className="projects">
+                            {skills.map((items, index) => {
+                                return (<li key={index}>
+                                    <a>
+                                        <div className="skills">
+                                            <img src={`${'./images/img/'}${items.img}`} />
+                                            <strong>{items.title}</strong>
+                                            <div className="cusProgress">
+                                                <div className="progress">
+                                                    <div className={`progress-bar ${items.bg}`}  role="progressbar" style={{ "width": `${items.progress}%` }} aria-valuenow={items.progress} aria-valuemin="0" aria-valuemax="100"></div>
+                                                </div>
+
+                                            </div>
+                                        </div>
+
+                                    </a>
+                                </li>)
+                            })}
+                        </ul>
+                    </div>
+
+
+                    <div className="boxPanel">
+                        <h4 className="text-center">Contact me</h4>
+                        <ul className="social_icons">
+                            {social.map((items, index) => {
+                                return (<li key={index}>
+                                    <a target={items.datatarget} href={items.datahref} rel={items.datarel[0], items.datarel[1]} title={items.title}><i className={items.icon[0] + " " + items.icon[1]}></i></a></li>)
+                            })}
+                        </ul>
+                    </div>
+
+                </div>
             </div>
         )
     }
@@ -70,13 +143,13 @@ class Footer extends React.Component {
 class ViomJeet extends React.Component {
     render() {
         return (
-            <div>
-                <section className="home_banner">
-                    <Header />
+            <section className="home_banner">
+                <Header />
+                <div className="bannerIn">
                     <Content />
-                </section>
+                </div>
                 <Footer />
-            </div>
+            </section>
         )
     }
 }
